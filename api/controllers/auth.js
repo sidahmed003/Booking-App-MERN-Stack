2e6b0e35f186d1c9babd7138953304b69f3b2909
sidahmed zerkani <sidahmedzerkani@gmail.com>
2025-03-16 16:42:37 +0100

    Cookie-parser added successfully

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -31,6 +31,8 @@ export const login = async (req,res,next) => {
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
         if (!isPasswordCorrect) return next(createError(400, "Wrong password or username!"));
 
+        const token = jwt.sign({ id:user._id, isAdmin:user.isAdmin }, process.env.JWT);
+
         const {password, isAdmin, ...otherDetails} = user._doc ;
         res.status(200).json({...otherDetails});
     } catch(err) {
