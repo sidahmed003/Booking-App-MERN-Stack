ec964440cd4b228728f3a84554e526ad08ad8c04
sidahmed zerkani <sidahmedzerkani@gmail.com>
2025-03-17 15:49:19 +0100

    Users API operations added successfully

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import authRoute from "./routes/auth.js"
 import usersRoute from "./routes/users.js"
 import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
-
+import cookieParser from "cookie-parser"
 
 const app = express() 
 
@@ -24,7 +24,8 @@ mongoose.connection.on("disconnected", () => {
     console.log("MongoDB disconnected!");
 })
 
-
+//middlewares
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
