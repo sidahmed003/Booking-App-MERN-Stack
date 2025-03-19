d7c533882d7b537e4ccc8139b8eb2ec1c3803bb1
sidahmed zerkani <sidahmedzerkani@gmail.com>
2025-03-19 16:21:51 +0100

    Hotels Routes updated

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,20 +2,21 @@ import express from "express"
 import Hotel from "../models/Hotel.js"
 import createError from "../utils/error.js"
 import {createHotel, deleteHotel, getHotel, getHotels, updateHotel} from "../controllers/hotel.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 //CREATE
 
-router.post("/", createHotel);
+router.post("/", verifyAdmin, createHotel);
 
 //UPDATE
 
-router.put("/:id", updateHotel)
+router.put("/:id", verifyAdmin, updateHotel)
 
 //DELETE
 
-router.delete("/:id", deleteHotel)
+router.delete("/:id", verifyAdmin, deleteHotel)
 
 //GET
 
