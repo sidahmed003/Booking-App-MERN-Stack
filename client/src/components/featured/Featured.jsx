7fdeae9be24782d7e20b91d6d42ded23bb9931a2
sidahmed zerkani <sidahmedzerkani@gmail.com>
2025-03-22 22:09:55 +0100

    useFetch Hook added successfully

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,6 +1,11 @@
+import useFetch from "../../hooks/useFetch.js";
 import "./featured.css";
 
 const Featured = () => {
+
+  const { data, loading, error} = useFetch("/hotels/countByCity?cities=berlin,madrid,london");
+  console.log(data)
+
   return (
     <div className="featured">
       <div className="featuredItem">
