2f9ea6ccf3eae1a62ec8addb072836af2c29555b
sidahmed zerkani <sidahmedzerkani@gmail.com>
2025-03-25 18:38:57 +0100

    PropertyList file updated

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,9 +1,14 @@
+import useFetch from "../../hooks/useFetch";
 import "./propertyList.css";
 
 const PropertyList = () => {
+
+  const { data, loading, error } = useFetch("http://localhost:5000/api/hotels/countByType");
+
   return (
     <div className="pList">
-      <div className="pListItem">
+      {loading ? ( "loading" ) : (
+      <><div className="pListItem">
         <img
           src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
           alt=""
@@ -57,7 +62,7 @@ const PropertyList = () => {
           <h1>Cabins</h1>
           <h2>2331 hotels</h2>
         </div>
-      </div>
+      </div></>)};-/
     </div>
   );
 };
